eda822e8e37a9f2b8fad35497a43989e846f5cce
sengathit <sengathit.l@gmail.com>
2020-01-11 18:33:11 -0600

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const appRoutes: Routes = [
-  { path: '', component: LoginComponent},
-  { path: 'dashboard', component: DashboardComponent}
+  { path: '', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'register', component: RegisterComponent }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
