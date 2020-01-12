14f200557a794cd894a83f6b5eb33f5314cddbac
sengathit <sengathit.l@gmail.com>
2020-01-11 18:57:47 -0600

    getting user

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -37,7 +37,7 @@ export class LoginComponent implements OnInit {
       this.loginCredential
     ).subscribe(data => {
       this.demo.username = true
-      this.users = data['rows']
+      this.users = data['rows'][0]
     },
     error => console.log(error))  
   }
